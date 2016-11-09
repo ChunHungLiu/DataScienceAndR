6df1c037e1254c150b2c2fb93a5a4dc541df6e79
Wush Wu <wush978@gmail.com>
2016-11-09 22:10:10 +0800

    finish main features

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var express = require("express");
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
-var gcloud = require('google-cloud');
-var datastore = gcloud.datastore;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(cookieParser());
 
-var datastoreClient = datastore();
-var key = datastoreClient.key('used_record');
+
+var gcloud = require('google-cloud')({
+  projectId : "datascienceandr"
+});
+var datastoreClient = gcloud.datastore();
 
 function errorPrinter(err) {
   console.error(JSON.stringify(err, ["message", "stack"]));
 }
 
-app.post("/status", function(req, res) {
+var router = express.Router();
+router.post("/status", function(req, res) {
+  var key = datastoreClient.key('used_record', Date.now());
   var obj = {
     user_id : req.body.user_id,
     course : req.body.course,
@@ -37,24 +45,64 @@ var urls = [
   "http://www.datascienceandr.org",
   "http://datascienceandr.org"
 ];
-app.post("/getManyRecords", function(req, res) {
-  if (err) {
-    errorPrinter(err);
+var entitiesCache = {
+  at : 0,
+  entities : []
+};
+router.post("/getManyRecords", function(req, res) {
+  for(url in urls) {
+    res.header("Access-Control-Allow-Origin", url);
+  }
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  var num;
+  if (isNaN(parseFloat(req.body.num))) {
+    num = 5;
   } else {
-    for(url in urls) {
-      res.header("Access-Control-Allow-Origin", url);
-    }
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.json({});
+    num = +(req.body.num || 5);
+  }
+  var query = datastoreClient.createQuery('used_record')
+    .order('created_at', {
+      descending : true
+    })
+    .limit(num)
+    ;
+  var now = Date.now();
+  if (now - entitiesCache.at > 1000 * 60) {
+    console.log("Query Google Datastore");
+    // update entities
+    return datastoreClient.runQuery(query, function(err, entities) {
+      if (err) {
+        errorPrinter(err);
+        return res.json({});
+      } else {
+        entitiesCache.at = Date.now();
+        entitiesCache.entities = entities;
+        return res.json(entitiesCache.entities);
+      }
+    });
+  } else {
+    return res.json(entitiesCache.entities);
   }
 });
 
-app.post("/getRecordsByUserId", function(req, res) {
-  res.json({});
+router.post("/getRecordsByUserId", function(req, res) {
+  var query = datastoreClient.createQuery('used_record')
+    .filter('user_id', '=', req.body.user_id)
+    ;
+  return datastoreClient.runQuery(query, function(err, entities) {
+    if (err) {
+      errorPrinter(err);
+      return res.json({});
+    } else {
+      return res.json(entities);
+    }
+  });
 });
 
+app.use('/api', router);
+
 // init server
-var server = app.listen(8081, function() {
+var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
