de92656c85c1897a19ae07cc1d1147d4ae157e2a
Wush Wu <wush978@gmail.com>
2016-11-10 13:47:03 +0800

    update

--- index.js
+++ index.js
@@ -102,38 +102,6 @@ router.post("/getRecordsByUserId", function(req, res) {
 app.use('/api', router);
 
 // init server
-var http = require('http');
-var port = 3000;
-app.set('port', port);
-var server = http.createServer(app);
-server.listen(port);
-server.on('error', function(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-});
-server.on('listening', function() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  console.log('Listening on ' + bind);
+var server = app.listen(process.env.PORT || '3000', function() {
+  console.log('App listening on port %s', server.address().port);
 });
