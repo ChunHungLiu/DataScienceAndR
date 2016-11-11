95123fa72063bb18638526df5e85d828e9d45067
Wush Wu <wush978@gmail.com>
2016-11-11 18:03:39 +0800

    add init script and update package.json for better logging

--- init.sh
+++ init.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 sudo apt-get update && \
- sudo apt-get install -y build-essential libssl-dev && \
- curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh && \
- nvm install v6.9.1 && \
- nvm default v6.9.1 && \
- npm install
+  sudo apt-get install -y build-essential libssl-dev nginx daemontools devscripts && \
+  # setting nginx
+  sudo cp nginx/default /etc/nginx/sites-available/default && \
+  sudo service nginx restart && \
+  # setting node js
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh && \
+  nvm install v6.9.1 && \
+  nvm default v6.9.1 && \
+  npm install
+
+rc=$?
+if [[ $rc != 0 ]]; then
+  exit $rc;
+fi
